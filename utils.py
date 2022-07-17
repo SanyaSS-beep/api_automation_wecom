fc5089e5772845b92f2b617d2bf58cf385d7df21
MiaSS-beep <maoshuaishuai0424@126.com>
2022-07-17 21:14:01 +0800

    omit too long str/bytes

--- utils.py
+++ utils.py
@@ -14,4 +14,20 @@ def lower_dict_keys(origin_dict):
     return {key.lower: value for key, value in origin_dict.items()}
 
 
+def omit_long_data(body, omit_len=512):
+    """omit too long str/bytes
+    """
+    if not isinstance(body, (str, bytes)):
+        return body
 
+    body_len = len(body)
+    if body_len <= omit_len:
+        return body
+
+    omitted_body = body[0:omit_len]
+
+    appendix_str = f" ... OMITTED {body_len - omit_len} CHARACTORS ... "
+    if isinstance(body, bytes):
+        appendix_str = appendix_str.encode("utf-8")
+
+    return omitted_body + appendix_str
